46124612eb6eea626977761dca6c7c197304b4d5
Crispin Agar <crispinagar.work+github@gmail.com>
2022-05-20 13:13:16 +0200

    Allowed sensors to detect traffic --> full simulation

--- main.js
+++ main.js
@@ -12,18 +12,18 @@ animate();
 
 function animate(){
   for(let i=0;i<traffic.length;i++){
-    traffic[i].update(road.borders);
+    traffic[i].update(road.borders,[]);
   }
-  car.update(road.borders);
+  car.update(road.borders,traffic);
   canvas.height=window.innerHeight;
   ctx.save();
   ctx.translate(0,-car.y+canvas.height*0.75);
   
   road.draw(ctx);
   for(let i=0;i<traffic.length;i++){
-    traffic[i].draw(ctx);
+    traffic[i].draw(ctx,"red");
   }
-  car.draw(ctx);
+  car.draw(ctx,"blue");
   ctx.restore();
   requestAnimationFrame(animate);
 }
