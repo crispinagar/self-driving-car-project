aa7ae5138d55d663a751fd3461b58bee2a9c9008
Crispin Agar <crispinagar.work+github@gmail.com>
2022-05-20 12:44:42 +0200

    Added collision detection

--- car.js
+++ car.js
@@ -19,7 +19,17 @@ class Car{
   update(roadBorders){
     this.#move();
     this.polygon=this.#createPolygon();
+    this.damaged=this.#assessDamage(roadBorders);
     this.sensor.update(roadBorders);
+  }
+
+  #assessDamage(roadBorders){
+    for(let i=0;i<roadBorders.length;i++){
+      if(polysIntersect(this.polygon, roadBorders[i])){
+        return true;
+      }
+    }
+    return false;
   }
 
   #createPolygon(){
@@ -93,6 +103,11 @@ class Car{
   }
 
   draw(ctx){
+    if(this.damaged){
+      ctx.fillStyle="red";
+    }else{
+      ctx.fillStyle="black";
+    }
     ctx.beginPath();
     ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
     for(let i=1;i<this.polygon.length;i++){
